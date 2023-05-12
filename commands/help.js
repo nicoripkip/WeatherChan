d36c1224dc94a3eb073638c8b54980ce8f982755
Nikoripkip <nicovanommen.nvo@gmail.com>
2023-05-12 19:29:22 +0200

    Tokens added

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,11 @@
 const { SlashCommandBuilder } = require("discord.js")
 
-
-const data = new SlashCommandBuilder()
-    .setName('help')
-    .setDescription('Gives back a list with all available commands!')
-
-
-async function execute(interaction){
-    await interaction.reply("Je moeder");
-}
-
-
 module.exports = {
-    data: data,
-    execute
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Gives back a list with all available commands!'),
+    
+    async execute(interaction) {
+        await interaction.reply("Je moeder")
+    }
 }
--- src/app.js
+++ src/app.js
@@ -5,33 +5,84 @@ const discord = require("discord.js")
 // const fetch = require("node-fetch")
 
 
+const commandPrefix = "!";
 const intents = {
     intents: [
-        discord.IntentsBitField.Flags.Guilds
+        discord.IntentsBitField.Flags.Guilds,
+        discord.IntentsBitField.Flags.GuildMessages,
+        discord.IntentsBitField.Flags.MessageContent,
     ]
 }
 const client = new discord.Client(intents)
 const commandsPath = path.join(__dirname, '../commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(commandsPath);
 
 
-client.on(discord.Events.ClientReady, () => {
+let commands = new discord.Collection()
+for (const file in commandFiles) {
+    const filePath = path.join(commandsPath, "help.js")
+    console.log(filePath)
+    const command = require(filePath)
+
+    console.log(command.data.name)
+
+    if ("data" in command && "execute" in command) {
+        commands.set(command.data.name, command)
+        console.log(`[henk]: ${commands.first().data}`)
+    } else {
+        console.log(`[warning]\tThe command at ${filePath} is missing a required [data] or [execute] tag`)
+    }   
+}
+
+client.on(discord.Events.ClientReady, client => {
     console.log(`[info]\tBot has logged in as: ${client.user.tag}`)
     console.log(`[info]\tInvite url: https://discord.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&permissions=${process.env.PERMISSIONS}&scope=bot%20applications.commands`)
+
+    client.guilds.cache.get(process.env.SERVER_ID).channels.cache.get(process.env.CHANNEL_ID).send("Henkers")
+})
+
+// and deploy your commands!
+// async function test() {
+// 	try {
+// 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+// 		// The put method is used to fully refresh all commands in the guild with the current set
+// 		// const data = await rest.put(
+// 		// 	discord.Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.SERVER_ID),
+// 		// 	{ 
+//         //         body: commands 
+//         //     },
+// 		// );
+
+// 		// console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+// 	} catch (error) {
+// 		// And of course, make sure you catch and log any errors!
+// 		console.error(error);
+// 	}
+// }
+
+// test()
+client.on('message', message => {
+    console.log(message.author.bot)
 })
 
 
-client.commands = new discord.Collection()
-for (const file in commandFiles) {
-    const filePath = path.join(commandsPath, file)
-    const command = require(filePath)
+client.on(discord.Events.InteractionCreate, async interaction => {
+    // if (!interaction.isChatInputCommand()) return;
 
-    if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command)
-    } else {
-        console.log(`[warning]\tThe command at ${filePath} is missing a required [data] or [execute] tag`)
+    console.log(interaction.commandName)
+
+    const command = interaction.client.commands.get(interaction.commandName)
+    if (!command) {
+        console.error(`[error]\tNo matching command: ${interaction.commandName} was found!\n`)
     }
-    
-}
+
+    try {
+        await command.execute(interaction)
+    } catch (error) {
+
+    }
+})
+
 
 client.login(process.env.TOKEN)
